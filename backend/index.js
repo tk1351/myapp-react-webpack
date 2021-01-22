0a7f355ba167d684e7343077e6a43d4429d83e4a
tk1351 <takakikitsui0111@gmail.com>
2021-01-22 10:28:45 +0900

    yarn add dotenv

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,13 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const http = require('http')
 const cors = require('cors')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const server = http.createServer(app)
 
 const PORT = process.env.PORT || 8080
-const config = require('./config/dev')
 
 const router = require('./routes')
 
@@ -17,7 +19,7 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-mongoose.connect(config.DB_URI, {
+mongoose.connect(process.env.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
